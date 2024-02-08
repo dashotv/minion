ef4ffc7d6915aa0c30c857fd14232ada35aab5b4
Shawn Catanzarite <me@shawncatz.com>
2024-02-08 11:18:24 -0800

    ScheduleFunc: only track job if it fails

--- minion.go
+++ minion.go
@@ -131,6 +131,27 @@ func (m *Minion) Schedule(schedule string, in Payload) (cron.EntryID, error) {
 	})
 }
 
+func (m *Minion) ScheduleFunc(schedule, name string, f func() error) (cron.EntryID, error) {
+	return m.cron.AddFunc(schedule, func() {
+		err := f()
+		if err != nil {
+			m.Log.Error(err)
+
+			data := &Model{
+				Args:   "{}",
+				Kind:   name,
+				Status: string(StatusFailed),
+				Queue:  "schedule_func",
+			}
+
+			err = m.db.Save(data)
+			if err != nil {
+				m.Log.Errorf("error saving job: %s", err)
+			}
+		}
+	})
+}
+
 // Remove removes a job from the cron scheduler.
 func (m *Minion) Remove(id cron.EntryID) {
 	m.cron.Remove(id)
